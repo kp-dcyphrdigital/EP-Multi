8b853abdde22ab95272025d0f764f0a1f1f5737b
Kazim P <kazim.parkar@syginteractive.com.au>
2018-05-15 09:07:19 +1000

    Environment dependent image generation

--- database/factories/CompetitionFactory.php
+++ database/factories/CompetitionFactory.php
@@ -3,13 +3,23 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Competition::class, function (Faker $faker) {
-    return [
-        'name' => $faker->words(2, true),
-        'slug' => $faker->word,
-        // 'banner' => $faker->image(base_path() . '/storage/app/public/images', 1920, 540, '', false),
-        'banner' => 'abc.jpg',
-		'cta' => $faker->sentence,
-        'description' => $faker->paragraphs(3, true),
-       	'terms' => $faker->paragraphs(10, true),
-    ];
+	if ( App::environment('testing') ) {
+	    return [
+	        'name' => $faker->words(2, true),
+	        'slug' => $faker->word,
+	        'banner' => 'abc.jpg',
+			'cta' => $faker->sentence,
+	        'description' => $faker->paragraphs(3, true),
+	       	'terms' => $faker->paragraphs(10, true),
+	    ];
+	} else {
+	    return [
+	        'name' => $faker->words(2, true),
+	        'slug' => $faker->word,
+	        'banner' => $faker->image(base_path() . '/storage/app/public/images', 1920, 540, '', false),
+			'cta' => $faker->sentence,
+	        'description' => $faker->paragraphs(3, true),
+	       	'terms' => $faker->paragraphs(10, true),
+	    ];		
+	}
 });
--- database/factories/EntryFactory.php
+++ database/factories/EntryFactory.php
@@ -3,16 +3,29 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Entry::class, function (Faker $faker) {
-    return [
-    	'competition_id' => App\Competition::inRandomOrder()->first()->id,
-		'firstname' => $faker->firstname(),
-		'lastname' => $faker->lastname,
-		'email' => $faker->email,
-		'telephone' => '04' . $faker->randomNumber(8, true),
-		// 'url' => $faker->image(base_path() . '/storage/app/public/images', 640, 480, '', false),
-		'url' => 'abc.jpg',
-        'approved' => rand(0, 1),
-        'created_at' => $faker->dateTimeBetween('-2 days', 'now', 'Australia/Sydney'),
-    ];
+	if ( App::environment('testing') ) {
+	    return [
+	    	'competition_id' => App\Competition::inRandomOrder()->first()->id,
+			'firstname' => $faker->firstname(),
+			'lastname' => $faker->lastname,
+			'email' => $faker->email,
+			'telephone' => '04' . $faker->randomNumber(8, true),
+			'url' => 'abc.jpg',
+	        'approved' => rand(0, 1),
+	        'created_at' => $faker->dateTimeBetween('-2 days', 'now', 'Australia/Sydney'),
+	    ];
+	} else {
+	    return [
+	    	'competition_id' => App\Competition::inRandomOrder()->first()->id,
+			'firstname' => $faker->firstname(),
+			'lastname' => $faker->lastname,
+			'email' => $faker->email,
+			'telephone' => '04' . $faker->randomNumber(8, true),
+			'url' => $faker->image(base_path() . '/storage/app/public/images', 640, 480, '', false),
+	        'approved' => rand(0, 1),
+	        'created_at' => $faker->dateTimeBetween('-2 days', 'now', 'Australia/Sydney'),
+	    ];		
+	}
+
 });
 
