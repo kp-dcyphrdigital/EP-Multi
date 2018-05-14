70ebd60e1a80f601983e61084e0717d8d3baadf9
Kazim P <kazim.parkar@syginteractive.com.au>
2018-05-14 17:40:52 +1000

    Seeder adjusted for prod

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" style="height: 100%;">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ENTREZO Promotions V2</title>
+    <!-- CSS -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
+    <link rel="icon" href="/favicon.ico">
+</head>
+<body style="height: 100%; display: flex; align-items: center; justify-content: center;">
+	<div style="width: 800px; text-align: center;">
+	    <h1>Automated entries are not allowed!</h1>
+	</div>
+</body>
+</html>
