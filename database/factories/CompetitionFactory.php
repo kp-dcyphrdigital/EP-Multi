7964cee7cbfecd924a1d3ae7aae6f6485d27eb83
Kazim P <kazim.parkar@syginteractive.com.au>
2018-05-14 17:06:43 +1000

    seeding for prod

--- database/factories/CompetitionFactory.php
+++ database/factories/CompetitionFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Competition::class, function (Faker $faker) {
     return [
         'name' => $faker->words(2, true),
         'slug' => $faker->word,
-        'banner' => $faker->image(base_path() . '/storage/app/public/images', 1920, 540, '', false),
+        'banner' => $faker->image('/home/master/applications/pmgrmwukyj/public_html/storage/app/public/images', 1920, 540, '', false),
         // 'banner' => 'abc.jpg',
 		'cta' => $faker->sentence,
         'description' => $faker->paragraphs(3, true),
