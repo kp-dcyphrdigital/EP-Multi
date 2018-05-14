040e527938ca040eb502bebba349d3ccec20a50c
Kazim P <kazim.parkar@syginteractive.com.au>
2018-05-14 16:37:26 +1000

    Enabling image seeding

--- database/factories/CompetitionFactory.php
+++ database/factories/CompetitionFactory.php
@@ -6,8 +6,8 @@ $factory->define(App\Competition::class, function (Faker $faker) {
     return [
         'name' => $faker->words(2, true),
         'slug' => $faker->word,
-        // 'banner' => $faker->image(base_path() . '/storage/app/public/images', 1920, 540, '', false),
-        'banner' => 'abc.jpg',
+        'banner' => $faker->image(base_path() . '/storage/app/public/images', 1920, 540, '', false),
+        // 'banner' => 'abc.jpg',
 		'cta' => $faker->sentence,
         'description' => $faker->paragraphs(3, true),
        	'terms' => $faker->paragraphs(10, true),
